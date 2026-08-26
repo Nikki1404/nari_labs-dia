f2a83f9e5f331b6f5d412a779a1e53985841e2c9
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-08-26 15:26:30 +0530

    Update update1.py

--- update1.py
+++ update1.py
@@ -3,17 +3,30 @@ $seeds=@(); while($seeds.Count -lt 100){$seeds+=Get-Random -Minimum 100 -Maximum
 python client.py --server http://localhost:8000 --text-file full_call.txt --reference-audio reference.wav --reference-text reference.txt --max-new-tokens 4096 --output full_call.wav --play
 
 
+(venv) PS C:\Users\re_nikitav\Documents\nari_labs-dia> python extract_voices.py --input-dir shortlisted\random_wavs --reference-text reference\reference.txt --agent-file shortlisted\random_wavs\seed_15423.wav --output-dir extracted_voices
+
 ================================================================================
-DIA TTS STARTUP
+REFERENCE
 ================================================================================
-Model             : nari-labs/Dia-1.6B-0626
-Device            : cuda
-PyTorch           : 2.6.0+cu124
-PyTorch CUDA      : 12.4
-CUDA available    : True
-GPU               : NVIDIA A10G
-DTYPE             : torch.float16
+S1 / Agent text   : Hello. Thank you for calling Inspira Financial. How can I help you today?
+S2 / Customer text: Hello. I need assistance with my account. Could you please help me with that?
 ================================================================================
-`torch_dtype` is deprecated! Use `dtype` instead!
-Fetching 2 files: 100%|██████████| 2/2 [01:38<00:00, 49.10s/it]
-ERROR:    Traceback (most recent call last):
+[CUSTOMER] seed_121.wav -> customer_seed_121.wav | boundary=3.74s
+[CUSTOMER] seed_1541.wav -> customer_seed_1541.wav | boundary=4.75s
+[CUSTOMER] seed_15423.wav -> customer_seed_15423.wav | boundary=4.13s
+[CUSTOMER] seed_15838.wav -> customer_seed_15838.wav | boundary=3.83s
+[CUSTOMER] seed_19094.wav -> customer_seed_19094.wav | boundary=3.55s
+[CUSTOMER] seed_21478.wav -> customer_seed_21478.wav | boundary=4.85s
+[CUSTOMER] seed_29108.wav -> customer_seed_29108.wav | boundary=3.14s
+[CUSTOMER] seed_33936.wav -> customer_seed_33936.wav | boundary=3.09s
+[CUSTOMER] seed_45718.wav -> customer_seed_45718.wav | boundary=4.90s
+[CUSTOMER] seed_687.wav -> customer_seed_687.wav | boundary=4.00s
+Traceback (most recent call last):
+  File "C:\Users\re_nikitav\Documents\nari_labs-dia\extract_voices.py", line 705, in <module>
+    main()
+    ~~~~^^
+  File "C:\Users\re_nikitav\Documents\nari_labs-dia\extract_voices.py", line 646, in main
+    raise FileNotFoundError(
+    ...<2 lines>...
+    )
+FileNotFoundError: Agent WAV not found: shortlisted\random_wavs\shortlisted\random_wavs\seed_15423.wav
